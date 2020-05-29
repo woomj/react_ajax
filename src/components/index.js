fa14fc51117877d13eaa7e05b4c61fe1021b939d
woomj <speedcross99@naver.com>
2020-05-29 09:55:29 +0900

    PostContainer

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import Header from "./Header/Header";
+import PostWrapper from "./PostWrapper/PostWrapper";
+export { Header, PostWrapper };
---
+++ src/containers/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { PostWrapper } from "../../components";
+
+class PostContainer extends Component {
+  render() {
+    return <PostWrapper>Hello, Post</PostWrapper>;
+  }
+}
+
+export default PostContainer;
